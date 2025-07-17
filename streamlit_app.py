3d0366d0e6d8954a449d3506bc572fc3b7d68ba2
Jenny <141550650+jenniii-om@users.noreply.github.com>
2025-07-17 11:56:35 +1000

    Update streamlit_app.py
    Expose JSON data from api

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ if ingredients_list:
 # New section to display fruityvice/smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
